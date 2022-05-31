41bb87cc87412125195e7acd3de805b9e5c1864e
Jeandre Awood <jeandreross@gmail.com>
2022-05-31 14:51:53 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Jeandre");
+
+        int myFirstNumber = 69;
+        int mySecondNumber = 666;
+        int myThirdNumber = 420;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println("My total is:" + " " + myLastOne);
+    }
+}
